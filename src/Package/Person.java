339521f2cbb566aa73913666afa90f6240951d35
Fistashka <tizilpe@gmail.com>
2020-12-16 18:25:31 +0200

    Initial commit

---
+++ src/Package/Person.java
@@ -0,0 +1,25 @@
+package Package;
+
+public class Person {
+
+    String name;
+
+    public class Account {
+
+        int password;
+
+        public void password() {
+
+            int password = 880055535;
+
+        }
+
+        public void print() {
+
+            System.out.println("Person name - " + name);
+
+        }
+
+    }
+    
+}
